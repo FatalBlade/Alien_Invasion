99bfed44d63f4bceadf23f95436cf8e2c7d66877
FatalBlade <Graves.Alexander.R@gmail.com>
2019-05-09 03:00:51 -0400

    Typo
    Removed extranious ")" from screen

--- alien_invasion.py
+++ alien_invasion.py
@@ -9,7 +9,7 @@ def run_game():
 		pygame.init()
 		ai_settings = Settings()
 		screen = pygame.display.set_mode(
-			(ai_settings.screen_width, ai_settings.screen_hight)))
+			(ai_settings.screen_width, ai_settings.screen_hight))
 		pygame.display.set_caption("Alien Invasion")
 		
 		# Start the main loop for the game.
