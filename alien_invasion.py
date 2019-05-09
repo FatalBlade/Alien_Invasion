e2dab03490d4e8deae97c6c1b273d13bc5d79e9d
FatalBlade <Graves.Alexander.R@gmail.com>
2019-05-09 02:46:25 -0400

    Imported Settings
    Imported the Settings class and deffered width, hight, and bg color to
    it.

--- alien_invasion.py
+++ alien_invasion.py
@@ -5,13 +5,12 @@ import pygame
 from settings import Settings
 
 def run_game():
-		# Initialize game and create a screen object.
+		# Initialize pygame, settings, and screen object.
 		pygame.init()
-		screen = pygame.display.set_mode((1900, 1040))
+		ai_settings = Settings()
+		screen = pygame.display.set_mode(
+			(ai_settings.screen_width, ai_settings.screen_hight)))
 		pygame.display.set_caption("Alien Invasion")
-		
-		# Set the background color.
-		bg_color = (230, 230, 230)
 		
 		# Start the main loop for the game.
 		while True:
@@ -22,7 +21,7 @@ def run_game():
 					sys.exit()
 					
 			# Redraw the screen during each pass through the loop.
-			screen.fill(bg_color)
+			screen.fill(ai_settings.bg_color)
 					
 			# Make the most recently drawn screen visible.
 			pygame.display.flip()
