5941bce631cc9e230cbd003eb76531065682fa05
FatalBlade <Graves.Alexander.R@gmail.com>
2019-05-09 02:34:26 -0400

    Revert "Main loop"
    This reverts commit 7c42e2941c47ece2f3c409e6c7cd5f65f1109f69.

--- settings.py
+++
@@ -1,9 +0,0 @@
-class Settings():
-		"""A class to store all settings for Alien Invasion."""
-		
-		def __init__(self):
-			"""Initialize the game's settings."""
-			# Screen settings
-			self.screen_width = 1900
-			self.screen_hight = 1040
-			self.bg_color = (230, 230, 230)
